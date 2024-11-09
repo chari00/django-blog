07d85ae27fb066f87586cbf9dd8fd6025f7362f3
chari00 <macharitocruzuk@gmail.com>
2024-11-09 23:52:49 +0000

    push updates for deployement

--- blogs/blog/models.py
+++ blogs/blog/models.py
@@ -2,6 +2,7 @@
 
 # # Create your models here.
 from django.db import models
+from django.utils import timezone
 
 # ----------------------------
 # Example: Defining a Simple Model
@@ -12,6 +13,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)  # Title of the post
     content = models.TextField()  # Content of the post
     published_date = models.DateTimeField(auto_now_add=True)  # Date of publication
-
+    def publish(self):
+        self.published_date = timezone.now()
     def __str__(self):
-        return self.title
+        return str(self.title)
--- blogs/blog/views.py
+++ blogs/blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-# Create your views here.
+# # Create your views here.
+# def index(request):
+#     return render(request, 'blog/index.html')
+
+
+from django.shortcuts import render
+from blog.models import Post
+from django.http import HttpResponse
+
+
+def post_list(request):
+    posts = Post.objects.all().order_by('-published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
